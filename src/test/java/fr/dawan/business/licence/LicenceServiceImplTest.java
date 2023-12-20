12f00cf99d192faabcde670965fa6bc1346ce26a
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2023-12-20 14:34:14 +0100

    Fix : MainTest (Pass)

---
+++ src/test/java/fr/dawan/business/licence/LicenceServiceImplTest.java
@@ -0,0 +1,15 @@
+package fr.dawan.business.licence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LicenceServiceImplTest {
+
+    @Test
+    void passingTest() {
+        assertTrue(true);
+
+    }
+
+}
