db3e3c9f190d0819d937e1194149cce1712cf2d6
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2023-12-22 10:41:49 +0100

    Feat : test index

---
+++ src/test/java/fr/dawan/business/home/HomeControllerTest.java
@@ -0,0 +1,13 @@
+package fr.dawan.business.home;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    void index() {
+        assertEquals("Hello !", new HomeController().index());
+    }
+}
