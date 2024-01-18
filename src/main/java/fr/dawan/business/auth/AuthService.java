a8c52f647938badff61bab54e54341bf8e487c8d
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2024-01-18 11:00:02 +0100

    Fix : remove auth & user package

--- src/main/java/fr/dawan/business/auth/AuthService.java
+++
@@ -1,6 +0,0 @@
-package fr.dawan.business.auth;
-
-public interface AuthService {
-    void register(RegisterDto registerDto);
-    LoginResponseDto login(LoginDto loginDto) throws Exception;
-}
--- src/main/java/fr/dawan/business/user/UserService.java
+++
@@ -1,6 +0,0 @@
-package fr.dawan.business.user;
-
-import fr.dawan.business.generic.GenericService;
-
-public interface UserService extends GenericService<UserDto> {
-}
