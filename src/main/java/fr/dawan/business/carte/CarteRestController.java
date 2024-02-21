36a31c141ebdc94dcf6000a1a38cc8a9d8edc725
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2024-02-21 16:40:17 +0100

    Feat : cors

--- src/main/java/fr/dawan/business/carte/CarteRestController.java
+++ src/main/java/fr/dawan/business/carte/CarteRestController.java
@@ -1,9 +1,11 @@
 package fr.dawan.business.carte;
 
 import fr.dawan.generic.AbstractGenericRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("api/v1/cartes")
 public class CarteRestController extends AbstractGenericRestController<CarteDto, CarteService> {
